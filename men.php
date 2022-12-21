c4527bf8f3ed16dafbab47f16779dc90976525f1
Radisami <121139549+Radisami@users.noreply.github.com>
2022-12-22 00:01:53 +0200

    Add files via upload

---
+++ men.php
@@ -0,0 +1,75 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> sudamrket</title>
+        <link rel="stylesheet" type="text/css" href="css\cont">
+    </head>
+<body>
+    
+    <nav class="menu">
+
+     <ul>
+      <li><a href="http://localhost/market/radi.php">Home</a></li>    
+      <li><a href="http://localhost/market/gallery.php">Gallery</a></li> 
+      <li><a href="http://localhost/market/help.php">Help</a></li>
+      <li><a href="http://localhost/market/about.php">About</a></li>
+      <li><a href="http://localhost/market/COURS1.php">Sign in</a></li>
+     </ul>
+    
+ <form class="search-form">
+     
+     <input type="text" placeholder="search">
+
+     <button>search</button>
+
+ </form>
+ 
+     <img style="width: 100px;float: right;margin-right: 20px;" src="img/rg.png"  >
+ 
+     
+     </nav>
+<br>
+<div class="Contact">
+    <table class="im">
+        <tr>
+          <td style="background-color: white;margin-left: 30px;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px;" src="img/images-22.jpg">
+            <h4>500$</h4> </td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-17.jpg"><h4>600$</h4></a></td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-20.jpg"><h4>550$</h4></a></td>
+        </tr>
+
+        <tr>
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-21.jpg"><h4>300$</h4></a></td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-26.jpg"><h4>250$</h4></a></td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-23.jpg"><h4>300$</h4></a></td>
+
+        </tr>
+        <tr>
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 
+            20px 20px 20px 20px" src="img/images-49.jpg"><h4>478$</h4></a></td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-24.jpg"><h4>198$</h4></a></td>
+
+            <td style="background-color:white;"><a href="#" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="#"><h4></h4></a></td>
+        </tr>
+    </table>
+    
+</div>
+<br>
+    <div class="footer">
+      <table style="margin-left: 580px;">
+          <tr>
+              <td><a href="#"><h4 style="color: white;">call as   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">politics   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">privacy     </h4></a></td>
+          </tr>
+      </table>  
+      <p style="margin-left:603px;font-size: 10px;">جميع الحقوق محفوظة للدفعة 11 شبكات 2022</p>
+    </div>
+    
+</body>    
+</html>
---
+++ women.php
@@ -0,0 +1,75 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> sudamrket</title>
+        <link rel="stylesheet" type="text/css" href="css\cont">
+    </head>
+<body>
+    
+    <nav class="menu">
+
+     <ul>
+      <li><a href="http://localhost/market/radi.php">Home</a></li>    
+      <li><a href="http://localhost/market/gallery.php">Gallery</a></li> 
+      <li><a href="http://localhost/market/help.php">Help</a></li>
+      <li><a href="http://localhost/market/about.php">About</a></li>
+      <li><a href="http://localhost/market/COURS1.php">Sign in</a></li>
+     </ul>
+    
+ <form class="search-form">
+     
+     <input type="text" placeholder="search">
+
+     <button>search</button>
+
+ </form>
+ 
+     <img style="width: 100px;float: right;margin-right: 20px;" src="img/rg.png"  >
+ 
+     
+     </nav>
+<br>
+<div class="Contact">
+    <table class="im">
+        <tr>
+          <td style="background-color: white;margin-left: 30px;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px;" src="img/images-1.jpg"><h4>1000$</h4> </td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-4.jpg"><h4>900$</h4></a></td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images.jpg"><h4>1500$</h4></a></td>
+        </tr>
+
+        <tr>
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-50.jpg"><h4>995$</h4></a></td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-52.jpg"><h4>2000$</h4></a></td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-51.jpg"><h4>3000$</h4></a></td>
+
+        </tr>
+        <tr>
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/img1.png"><h4>1000$</h4></a></td>
+
+            <td style="background-color:white;"><a href="http://localhost/market/pay" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/img2.png"><h4>1800$</h4></a></td>
+
+            <td style="background-color:white;"><a href="#" style="text-decoration: none;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="#"><h4></h4></a></td>
+        </tr>
+    </table>
+    
+</div>
+    
+</div>
+<br>
+    <div class="footer">
+      <table style="margin-left: 580px;">
+          <tr>
+              <td><a href="#"><h4 style="color: white;">call as   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">politics   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">privacy     </h4></a></td>
+          </tr>
+      </table>  
+      <p style="margin-left:603px;font-size: 10px;">جميع الحقوق محفوظة للدفعة 11 شبكات 2022</p>
+    </div>
+    
+</body>    
+</html>
